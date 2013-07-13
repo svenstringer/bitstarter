6c833b1d981e4b6ce106a4a8bdbae058f64cc25c
Sven <svenstringer@server.fake>
2013-07-13 17:17:34 +0000

    add buffer read to web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World22!');
+  var buffer = fs.readFile('index.htm');
+  var text = buf.toString('utf8');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
