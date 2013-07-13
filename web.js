f7cbeeea24c9f482c7b16c8115857090bc471b16
Sven <svenstringer@server.fake>
2013-07-13 17:24:27 +0000

    use sync fileread in web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFile('index.htm');
+  var buffer = fs.readFileSync('index.htm');
   var text = buffer.toString('utf8');
   response.send(text);
 });
