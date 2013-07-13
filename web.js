f4df0749b35eb0ce77e1d9d4ce6aad6c8ff552f0
Sven <svenstringer@server.fake>
2013-07-13 17:21:03 +0000

    fix buffer var typo in web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = fs.readFile('index.htm');
-  var text = buf.toString('utf8');
+  var text = buffer.toString('utf8');
   response.send(text);
 });
 
