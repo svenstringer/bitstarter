81461705056cf51d939af3bb0285b7ea06dbb29c
Sven <svenstringer@server.fake>
2013-07-13 17:25:51 +0000

    fix htm(l) typo in file name

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.htm');
+  var buffer = fs.readFileSync('index.html');
   var text = buffer.toString('utf8');
   response.send(text);
 });
